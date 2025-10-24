d261d3a602e33b34f000b20512a4049e9e1b89e6
achmadraja <achmadrajaqz@gmail.com>
2025-10-24 11:05:34 +0700

    feat(api) : add axios instance endpoint

---
+++ frontend/src/api/axiosInstance.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/api"; // ganti sesuai port backend kamu
+
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default axiosInstance;
